b9ae025e05d59249af914d86a10edfe9e7a319e3
Lars Pasman <95089151+LarsPasman@users.noreply.github.com>
2022-08-25 17:51:10 +0000

    Window Size

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-let colorlist = ['gold', 'turquoise', 'red']
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  var cnv = createCanvas(windowWidth - 15, windowHeight - 20);
+  cnv.style('display', 'block');
+
+  rectMode(CENTER);
+  
 }
 
-function draw() {
-  noStroke()
-  fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
+
+function draw(){
+  background(0);
+
+  stroke(0,255,0);
+  noFill();
+  strokeWeight(3);
+  rect(width/2, height/2, width, height);
+  noStroke();
+  fill(0,255,0);
+  rect(width/2, 25, width, 50);
+  }
